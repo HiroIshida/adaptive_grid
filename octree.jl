0041c75e935b4397f95f33897d4eb78bb1ba848e
HiroIshida <spitfire.docomo@gmail.com>
2019-03-06 16:07:40 +0900

    [src] show contour for tree

--- octree.jl
+++ octree.jl
@@ -258,3 +258,57 @@ function pred_standard(node::Node, f, ε, n_grid, itp_method)
     return ~(max_error<ε)
 end
 
+function show_contour2(tree::Tree; axis_slice=1, pos_slice=0, N_cell=300)
+    b_min = tree.node_root.b_min
+    b_max = tree.node_root.b_max
+    step = (b_max - b_min)./N_cell
+    data_plot = zeros(N_cell, N_cell)
+    for i in 1:N_cell, j in 1:N_cell
+        p_cell = b_min + [i-0.5, j-0.5].*step
+        val = evaluate(tree, p_cell)
+        data_plot[i, j] = val
+    end
+    extent = [b_min[1] + 0.5*step[1],
+              b_max[1] - 0.5*step[1],
+              b_min[2] + 0.5*step[2],
+              b_max[2] - 0.5*step[2]]
+    PyPlot.imshow(data_plot,
+                  extent = extent,
+                  interpolation=:bicubic)
+    show()
+
+end
+
+function show_contour3(tree::Tree; axis_slice=1, pos_slice=0, N_cell=300)
+    # consider right hand 
+    if axis_slice == 1
+        plot_axis_1 = 2; plot_axis_2 = 3
+    elseif axis_slice == 2
+        plot_axis_1 = 3; plot_axis_2 = 1
+    else
+        plot_axis_1 = 1; plot_axis_2 = 2
+    end
+
+    b_min_ = tree.node_root.b_min
+    b_max_ = tree.node_root.b_max
+
+    b_min = [b_min_[plot_axis_1], b_min_[plot_axis_2]]
+    b_max = [b_max_[plot_axis_1], b_max_[plot_axis_2]]
+    step = (b_max - b_min)./N_cell
+
+    data_plot = zeros(N_cell, N_cell)
+    for i in 1:N_cell, j in 1:N_cell
+        p_cell = b_min + [i-0.5, j-0.5].*step
+        p_cell_3d = vcat(pos_slice, p_cell)
+        val = evaluate(tree, p_cell_3d)
+        data_plot[i, j] = val
+    end
+    extent = [b_min[1] + 0.5*step[1],
+              b_max[1] - 0.5*step[1],
+              b_min[2] + 0.5*step[2],
+              b_max[2] - 0.5*step[2]]
+    PyPlot.imshow(data_plot,
+                  extent = extent,
+                  interpolation=:bicubic)
+    show()
+end
